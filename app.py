9ad97d4b7f038eca7d716046e6c3a17757374644
mannan1821 <gamersage1810@gmail.com>
2026-08-01 09:44:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,150 @@
+# Step 1 =========== Load Modules =============
 
+import os
+import time
+import langchain
+from langchain.agents import create_agent
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_groq import ChatGroq
+import pytesseract as pyt
+from tavily import TavilyClient
+import numpy as np
+import streamlit as st
+
+#============ API KEYS AND ENV VARIABLES ============
+GOOGLE_API_KEY = "AQ.Ab8RN6K8rqxU64adnAmfxNtcyxv0reQcq_nOWEng4a_SYR-4dA"
+GROQ_API_KEY = "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"
+TAVILY_API_KEY = "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"
+
+st.title("Agentic PPT Generator")
+st.header("User can generate, PPT, Images, and fetch Latest News")
+
+st.sidebar.title("Give API KEYS")
+
+all_API = [TAVILY_API_KEY, GROQ_API_KEY, GOOGLE_API_KEY]
+if not all(all_API):
+    st.sidebar.error("Must give API keys")
+    
+    url = "https://aistudio.google.com/api-keys"
+    st.markdown(f"Get Google API key-{url}")
+
+    url ="https://app.tavily.com/playground"
+    st.markdown(f"Get Tavily API key-{url}")
+
+elif all(all_API):
+    st.success("API KEYS LOADED SUCCESSFULLY")
+    options = ["gemini-3.5-flash-lite", "gemini-3.5-flash"
+              "gemini-2.5-flash-lite", "gemini-2.5-flash"]
+
+    selected_model = st.selectbox("Select-Model", options = options)
+  
+    model = ChatGoogleGenerativeAI(
+    model = 'gemini-3.5-flash-lite',
+    google_api_key = GOOGLE_API_KEY
+    )
+else:
+    st.info("TRY VALID API_KEYS")
+
+model = ChatGoogleGenerativeAI(
+    model = 'gemini-3.5-flash-lite',
+    google_api_key = GOOGLE_API_KEY
+)
+
+# response = model.invoke("Hello Buddy!")
+# response.content[-1]["text"]
+
+def search_latest_info(query):
+  """This function helps to give lastest
+  search using tavily based on given user query
+  related research or contents"""
+
+  client = TavilyClient(
+      api_key  = TAVILY_API_KEY)
+
+  response = client.search(query)
+  return response
+
+search_latest_info("String Theory in Quantum Mechanics")
+
+# Tool 2
+def generate_image(img_prompt,slide_no = 1):
+  """This function helps user to generate
+  image using free api, with given
+  img_prompt, with slide no"""
+
+  url = f"https://image.pollinations.ai/{img_prompt}"
+
+  import requests as r
+  content = r.get(url).content
+  with open(f"ai_image_{slide_no}.jpeg",'wb') as f:
+    f.write(content)
+
+  from PIL import Image
+  img = Image.open(f"ai_image_{slide_no}.jpeg")
+  return img
+
+generate_image("A carrot holding a baseball bat and has human eyes, eybrows, teeth and mouth")
+
+# Agent Creation
+leader_agent = create_agent(
+    model = model,
+    tools = [search_latest_info,
+             #generate_image
+             ])
+# leader_agent
+
+def run_agent(agent, query):
+  """This is the main agent, or leader agent
+  orchestrate sub agents"""
+
+  # Giving prompt to create detailed prompt
+  # for code generation
+  prompt = f"""Based on Below given Query,
+  your task is to call specific tool, first to
+  promptify user prompt, than call image tool, or
+  latest search if required.give slide dynamic, ui ux,
+  with creative design, keep help of function to generate image
+  based on given topic,
+  Generate image using
+  with number of slide asked, and use time sleep to hit image request on server
+  and using file handling embed this in output html, use java script function
+  give Final response output in HTML, no markdowns
+  user query given below:
+  """
+  prompt = prompt+query
+
+  # prompt = agent_prompt(prompt)
+
+  response = leader_agent. invoke({'messages' : [ {'role':'user',
+                                                   'content' :prompt} ]})
+  code = response['messages'][-1].content[-1]['text']
+  return code
+
+user = """Create ppt for presenter Osama Bin Laden
+Topic: 911 - American Tragedy
+10 slides"""
+code = run_agent(leader_agent, user)
+ip.display.HTML (code)
+
+if st.button("Generate Resume"):
+    with st.spinner("al-qaeda initiated"):
+        raw_code = main_agent(legend_agent, user_info)
+        
+        code = raw_code.replace("```html", "").replace("```", "").strip()
+        
+        st.html(code, width="stretch", unsafe_allow_javascript=True)
+        
+        st.divider() # to give horizontal div
+    
+        raw_job_code = get_jobs(agent, location, profile)
+        job_code = raw_job_code.replace("```html", "").replace("```", "").strip()
+        
+        st.html(job_code, width="stretch", unsafe_allow_javascript=True)
+
+# File save
+with open("ppt.html", 'w') as f:
+  f.write(code)
+
+# Only for colab user to download file
+from google.colab import files
+files.download("ppt.html")
