c0c4daaa17d751b8ade4394991ab41715c4119f0
mannan1821 <gamersage1810@gmail.com>
2026-08-01 10:57:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ elif all(all_API):
     selected_model = st.selectbox("Select-Model", options = options)
   
     model = ChatGoogleGenerativeAI(
-    model = 'gemini-2.5-flash',
+    model = 'gemini-3.5-flash',
     google_api_key = GOOGLE_API_KEY
     )
 else:
